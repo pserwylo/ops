49d2aa30cca7fedea7b0c43b5e8820bc4b071b8f
Peter Serwylo <pete@feebie.(none)>
2010-05-11 07:43:27 +1000

    Clickrobot: Added countdown to allow it to stop after a certain amount of time.

--- src/com/serwylo/ops/clickrobot/gui/ClickRobotGui.java
+++ src/com/serwylo/ops/clickrobot/gui/ClickRobotGui.java
@@ -19,7 +19,7 @@ public class ClickRobotGui extends JFrame implements ActionListener
 	private static final long serialVersionUID = -8580140189070468490L;
 
 	private JLabel labelError;
-	private JSpinner spinnerInterval;
+	private JSpinner spinnerInterval, spinnerCountdown;
 	private JToggleButton btnToggleStart;
 	
 	private Timer timer;
@@ -29,12 +29,10 @@ public class ClickRobotGui extends JFrame implements ActionListener
 		this.timer = new Timer( 1000, this );
 
 		this.setTitle( "Fred the Mouse" );
-		this.setLayout( new BorderLayout() );
-		this.setMinimumSize( new Dimension( 220, 100 ) );
+		this.setLayout( new GridBagLayout() );
+		this.setMinimumSize( new Dimension( 250, 130 ) );
 		this.setDefaultCloseOperation( EXIT_ON_CLOSE );
 
-		JPanel panel = new JPanel();
-		panel.setLayout( new FlowLayout() );
 		this.spinnerInterval = new JSpinner( new SpinnerNumberModel( 1000, 100, Integer.MAX_VALUE, 100 ) );
 		this.spinnerInterval.setValue( 1000 );
 		this.spinnerInterval.setPreferredSize( new Dimension( 80, 20 ) );
@@ -49,19 +47,42 @@ public class ClickRobotGui extends JFrame implements ActionListener
 				}
 			}
 		);
-		panel.add( new JLabel( "Interval (ms): " ) );
-		panel.add( this.spinnerInterval );
-		this.add( panel, BorderLayout.NORTH );
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.insets = new Insets( 4, 4, 4, 4 );
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		this.add( new JLabel( "Interval (ms): " ), constraints );
 		
-		panel = new JPanel();
+		constraints.gridx = 1;
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		this.add( this.spinnerInterval, constraints );
+
+		
+		this.spinnerCountdown = new JSpinner( new SpinnerNumberModel( 30000, 1000, Integer.MAX_VALUE, 1000 ) );
+		this.spinnerCountdown.setValue( 30000 );
+		this.spinnerCountdown.setPreferredSize( new Dimension( 80, 20 ) );
+		
+		constraints = new GridBagConstraints();
+		constraints.insets = new Insets( 4, 4, 4, 4 );
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		this.add( new JLabel( "Stop in (ms): " ), constraints );
+		
+		constraints.gridx = 1;
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		this.add( this.spinnerCountdown, constraints );
+		
+		JPanel panel = new JPanel();
 		panel.setLayout( new GridBagLayout() );
 		this.btnToggleStart = new JToggleButton( "Start" );
 		this.btnToggleStart.addActionListener( this );
-		panel.add( this.btnToggleStart );
-		this.add( panel, BorderLayout.CENTER );
+		panel.add( this.btnToggleStart, new GridBagConstraints() );
 		
-		this.labelError = new JLabel();
-		this.add( this.labelError, BorderLayout.SOUTH );
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.gridx = 0; constraints.gridy = 2;
+		constraints.gridwidth = 2;
+		constraints.insets = new Insets( 4, 4, 4, 4 );
+		this.add( panel, constraints );
 		
 		this.setVisible( true );
 		
@@ -90,17 +111,28 @@ public class ClickRobotGui extends JFrame implements ActionListener
 		}
 		else if ( e.getSource() == this.timer )
 		{
+			
 			try
 			{
+				this.spinnerCountdown.setValue( (Integer)spinnerCountdown.getValue() - (Integer)spinnerInterval.getValue() );
 				Robot robot = new Robot();
 				robot.mousePress( InputEvent.BUTTON1_MASK );
 				robot.mouseRelease( InputEvent.BUTTON1_MASK );
 			}
 			catch ( AWTException awte )
 			{
-				this.labelError.setText( "Error attempting to click:\n" + awte.getMessage() );
+				JOptionPane.showMessageDialog( this, "Error attempting to click:\n" + awte.getMessage() );
 				awte.printStackTrace();
 				this.timer.stop();
+				this.btnToggleStart.setText( "Start" );
+				this.btnToggleStart.setSelected( false );
+			}
+		
+			if ( (Integer)this.spinnerCountdown.getValue() <= 0 )
+			{
+				this.timer.stop();
+				this.btnToggleStart.setText( "Start" );
+				this.btnToggleStart.setSelected( false );
 			}
 		}
 	}
